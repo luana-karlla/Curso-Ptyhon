e1c55886f4d1f04a0944f3c962fee70c4520f43e
Luana Karlla Silva <karlla_1911@hotmail.com>
2022-04-12 21:37:14 -0300

    Python
    Exercicios  aula 1 a 4 -  treino

---
+++ Aula-4/aula-04.py
@@ -0,0 +1,29 @@
+#print('Olá Mundo!')
+#print(7+10)
+#print('7'+'1')
+#============================================
+#nome = input('Seu nome? ')
+#idade = input('Sua idade? ')
+#peso = input('Seu peso? ')
+#esCivil = input('Voce é casada? ')
+
+#print(nome,idade,peso,esCivil)
+#=======================================================
+#nome = input('Qual é o seu nome? ')
+#print('Ola, ', nome,' seja muito bem vindo(a)')
+#=======================================================
+#dia = input('Digite dia de nasci.: ')
+#mes = input('Digite seu mês de nasci.: ')
+#ano = input('Digite o ano que voce nasceu: ')
+#print('Sua data de nascimento é: ', dia,'/',mes,'/',ano)
+#=======================================================
+
+soma1 = int (input('numero 1: '))
+soma2 = int (input('numero 2: '))
+
+resul  = (soma1+soma2)
+
+print(resul)
+
+
+
