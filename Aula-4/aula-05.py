9c20cdb99a69483d3b00582fc787596bd6bbb5fc
Luana Karlla Silva <karlla_1911@hotmail.com>
2022-04-18 17:21:17 -0300

    Aula 05
    Na verdade é a aula 06 - tipos primitivos

---
+++ Aula-4/aula-05.py
@@ -0,0 +1,17 @@
+#n1 = int (input('diginte numero: '))
+#n2 = int (input('digite outro numero: '))
+#s = n1+n2
+#print(f'A soma de {n1} e {n2} é {s}')
+
+#n1 = float(input('Digite um numero: '))
+#n2 = float(input('digite outro numero: '))
+#s = n1+n2
+#print(f'a soma de {n1} e {n2} é {s}')
+
+
+
+from typing import Type
+
+
+n1 = (input('digite qualquer coisa: '))
+print(Type(n1))
