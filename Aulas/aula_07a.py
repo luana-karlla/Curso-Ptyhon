06c9b6be9417231d4c51440123e0ccb96375eaa1
Luana Karlla Silva <karlla_1911@hotmail.com>
2022-04-18 20:37:07 -0300

    Aula 07
    exercicio em andamento

---
+++ Aulas/aula_07a.py
@@ -0,0 +1,13 @@
+#nome = input("Qual seu nome: ")
+#print(f'Seu nome é: {nome}')
+#print(f'Olá, {nome:=^20}'.upper())
+
+n1 = int(input('Digite numero: '))
+n2 = int(input('Digite outro numero: '))
+s= n1+n2
+d= n1/n2
+m= n1*n2
+di= n1//n2
+e= n1**n2
+print(f'o resultado da soma {s}, \n do produto {m}, da divisao {d} ', end=' ')
+print(f'Divisao inteira {di}, e potencia {e}')
