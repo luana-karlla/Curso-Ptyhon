dadbd20282ca919416f5b71d5a4f74baff4f21eb
Luana Karlla Silva <karlla_1911@hotmail.com>
2022-05-17 18:46:40 -0300

    Aula 10
    -Condição;
    -Exemplos.

---
+++ Aulas/aula10.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual é o seu nome: '))
+if nome == 'Luana':
+    print('Que lindo nome voce tem!')
+else:
+    print('Seu nome é tão normal!')
+print(f'Bom dia, {nome}.')
---
+++ Aulas/aula10a.py
@@ -0,0 +1,9 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m=(n1+n2)/2
+print(f'A sua média foi de: {m}')
+
+if m>=6.0:
+    print('Sua média foi boa, PARABÉNS!!!')
+else:
+    print('Você não foi bem, precisa estudar mais!!!')
