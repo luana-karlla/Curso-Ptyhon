a91fc72b72610ae973f3496bd261b453b064170c
Luana Karlla Silva <karlla_1911@hotmail.com>
2022-05-24 15:51:17 -0300

    Adicionando cores no terminal

--- Mundo1-Fundamentos/desafio_05.py
+++ Mundo1-Fundamentos/desafio_05.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
 
-n = int(input('Digite um número: '))
+n = int(input('\033[32mDigite um número: \033[m'))
 ant = n-1
 suc = n+1
-print(f'O antecessor de {n}, é {ant} e o sucessor é {suc}')
+print(f'\033[43mO antecessor de {n}, é {ant} e o sucessor é {suc}\033[m')
--- Mundo1-Fundamentos/desafio_06.py
+++ Mundo1-Fundamentos/desafio_06.py
@@ -1,7 +1,7 @@
 #Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada
 
-n = int(input('Digite um número: '))
+n = int(input('\033[4;35mDigite um número:\033[m'))
 d = n*2
 t = n*3
 r = n**(1/2)
-print(f'O dobro de {n}, é {d}, o triplo é {t} e a raiz quadrada é {r}')
+print(f'\033[1;30;44mO dobro de {n}, é {d}, o triplo é {t} e a raiz quadrada é {r}\033[m')
